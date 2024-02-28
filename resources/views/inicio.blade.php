006cd8e24331a236dbec4335b69afe0ded1bd73d
lllHunterlllSV <ia.edwin_hernandez@ufg.edu.sv>
2024-02-28 15:42:51 -0600

    mejora de estilo

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -4,7 +4,7 @@
 <div class="container col-xxl-8 px-4 py-5">
     <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
       <div class="col-10 col-sm-8 col-lg-6">
-        <img src="{{ asset('img/index-image.jpg') }}" class="d-block mx-lg-auto img-fluid shadow-lg" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+        <img src="{{ asset('img/index-image.jpg') }}" class="d-block mx-lg-auto img-fluid shadow-lg" alt="Facturacion electronica" width="700" height="500" loading="lazy">
       </div>
       <div class="col-lg-6">
         <h1 class="display-5 fw-bold lh-1 mb-3">La Facturación electrónica es el futuro del país</h1>
