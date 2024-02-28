c29fecea561df15f2dbf0f03b5d40dfe90ae64e7
lllHunterlllSV <ia.edwin_hernandez@ufg.edu.sv>
2024-02-28 15:06:32 -0600

    primer commit

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container col-xxl-8 px-4 py-5">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img src="{{ asset('img/index-image.jpg') }}" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold lh-1 mb-3">La Facturación electrónica es el futuro del país</h1>
+        <p class="lead">Por eso es importante la creación de plantillas que optimicen todo el proceso, esta aplicación se encarga de que cualquier usuario pueda emitir un DTE utilizando una interfaz y funcionamiento intuitivo.</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Aprende más aquí</button>
+         
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite([ 'resources/js/app.js', 'resources/css/app.scss'])
+    <script src="/main.js"></script>
+    <script src="https://kit.fontawesome.com/9f312215fd.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg menu mb-5">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white" href="#"><img src="/imagenes/factura.png" alt="logo" width="45px"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav" id="listamenu">
+        <li class="nav-item ms-3">
+          <a class="nav-link text-white" href="">Historial. Tokens</a>
+        </li>
+        <li class="nav-item ms-3">
+          <a class="nav-link text-white" href="">Historial. Emisor</a>
+        </li>
+        <li class="nav-item ms-3">
+          <a class="nav-link text-white" href="">Historial. Receptor</a>
+        </li>
+        <li class="nav-item ms-3">
+          <a class="nav-link text-white" href="">Historial. DTE</a>
+        </li>
+        <li class="nav-item ms-3">
+          <a class="nav-link text-white" href="">Eventos de contingencia</a>
+        </li>
+        <li class="nav-item ms-3">
+          <a class="nav-link text-white" href="">Eventos de invalidación</a>
+        </li>
+        
+        
+      </ul>
+    </div>
+
+    <div class="btn-group">
+      
+      <a class="nav-link text-white " href="">Envios de DTE</a>
+      
+      <div class="ms-3 dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Usuario
+
+      </div>
+
+  <ul class="dropdown-menu dropdown-menu-end">
+    <li><button class="dropdown-item" type="button">Configuración</button></li>
+    <li><button class="dropdown-item" type="button">Soporte Tecnico</button></li>
+    <li><button class="dropdown-item" type="button">Decimales</button></li>
+    <li><a href="" class="dropdown-item" type="button">Seccion de Token</a></li>
+  </ul>
+</div>
+    
+  </div>
+</nav>
+
+
+@yield('content')
+</body>
+</html>
